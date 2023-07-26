bcfb182ecb8e0ec0be7f4c3db7efdfb80c732f8f
jswhite1992 <jswhite1992@gmail.com>
2023-07-25 21:04:43 -0500

    adding final code

--- pychain.py
+++ pychain.py
@@ -49,7 +49,12 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender: str
+    receiver: str
+    amount: float
+
 
 
 ################################################################################
@@ -166,19 +171,19 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+# input_data = st.text_input("Block Data")  # We commented out this line to delete the variable
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input("Sender")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input("Receiver")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.number_input("Amount", min_value=0.0, step=0.01)
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
@@ -189,13 +194,14 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
+        record=Record(sender, receiver, amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
 
     pychain.add_block(new_block)
     st.balloons()
+
 
 ################################################################################
 # Streamlit Code (continues)
